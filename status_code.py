eb22181f090b96aff957d34fe113282eb5606724
larago <bingeye@foxmail.com>
2017-01-27 13:29:40 +0800

    more details about status code

--- status_code.py
+++ status_code.py
@@ -4,3 +4,10 @@ import requests
 
 r = requests.get('http://httpbin.org/get')
 print r.status_code
+print requests.codes.ok
+print requests.codes.ok == r.status_code
+print r.raise_for_status()
+
+bad_r = requests.get('http://httpbin.org/status/404')
+print bad_r.status_code
+bad_r.raise_for_status()
